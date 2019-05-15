0fddf3e7475ffde8c9a94d4503b1ed5c7782cbcd
ElioPez <jetasagames@gmail.com>
2019-05-15 12:49:34 -0300

    Algunos Modelos modificados

--- app/Categoria.php
+++ app/Categoria.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
 	protected $table = "categorias";
+    protected $primaryKey ="id";
 
 	protected $filable = ['nombre','descripcion'];
 }
--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+	protected $table = "productos";
+    protected $primaryKey ="id";
+
+	protected $filable = [
+        'producto',
+        'descripcion',
+        'precio',
+        'imagen',
+        'stock',
+        'categoria_id'
+    ];
 }
